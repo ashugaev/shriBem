89c62a5c7ffd3390379bd446404c9c8bb2d8cc75
Шугаев Алексей Андреевич <a.shugaev@carprice.ru>
2018-11-13 00:07:04 +0300

    bem className

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,29 +1,32 @@
 import React from "react";
 import "./index.scss";
+import { cn } from "@bem-react/classname";
+
+export const cnFooter = cn("footer");
 
 function Footer() {
   return (
-    <footer class="footer">
-      <div class="footer__menu">
-        <div class="footer__menu-item">
+    <footer className={cnFooter()}>
+      <div className={cnFooter("menu")}>
+        <div className={cnFooter("menuItem")}>
           <a href="#">Помощь</a>
         </div>
-        <div class="footer__menu-item">
+        <div className={cnFooter("menuItem")}>
           <a href="#">Обратная связь</a>
         </div>
-        <div class="footer__menu-item">
+        <div className={cnFooter("menuItem")}>
           <a href="#">Разработчикам</a>
         </div>
-        <div class="footer__menu-item">
+        <div className={cnFooter("menuItem")}>
           <a href="#">Условия пользования</a>
         </div>
-        <div class="footer__menu-item">
+        <div className={cnFooter("menuItem")}>
           <a href="/src/files/presentation.pdf" target="_blanck">
             Ссылка на лицензию
           </a>
         </div>
       </div>
-      <div class="footer__copy">| © 2001-2018 ООО «Яндекс»</div>
+      <div className={cnFooter("copy")}>| © 2001-2018 ООО «Яндекс»</div>
     </footer>
   );
 }
